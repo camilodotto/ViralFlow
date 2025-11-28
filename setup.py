16befc0e0a2eba4ee2220cf8a538c9fd9014ef31
Camilo Reis Dotto <camilo.dotto@dataprev.gov.br>
2025-11-28 15:11:30 -0300

    cfg setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='ViralFlow',
-      version='1.3.4',
-      description='''
-      Workflows for viral genome Assembly at FioCruz/IAM
-      ''',
-      url='https://github.com/dezordi/ViralFlow/',
-      author='Antonio Marinho & Filipe Z. Dezordi',
-      author_email='amarinhosn@pm.me & zimmer.filipe@gmail.com',
-      py_modules = [],
-      classifiers=[
+setup(
+    name='ViralFlow',
+    version='1.3.4',
+    description='''
+    Workflows for viral genome Assembly at FioCruz/IAM
+    ''',
+    url='https://github.com/dezordi/ViralFlow/',
+    author='Antonio Marinho & Filipe Z. Dezordi',
+    author_email='amarinhosn@pm.me & zimmer.filipe@gmail.com',
+    # aqui é o ponto importante:
+    packages=['wrapper'],
+    # ou, se quiser algo mais geral:
+    # packages=find_packages(include=['wrapper', 'wrapper.*']),
+    classifiers=[
         'Programming Language :: Python :: 3.8',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics'
-          ],
-      scripts=['wrapper/viralflow'],
-      zip_safe=False
+    ],
+    scripts=['wrapper/viralflow'],
+    zip_safe=False,
 )
